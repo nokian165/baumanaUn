9085e9b7019ee5cd86b489cd95500c89b30c8e62
User-PC\User <redchukroma@gmail.com>
2018-04-21 10:07:07 +0300

    Add project files.

---
+++ baumanaUn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="baumanaUn.MvcApplication" Language="C#" %>
---
+++ baumanaUn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(baumanaUn.Startup))]
+namespace baumanaUn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
